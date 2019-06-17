6115129905368d180dee6fa7a8c72a00f627388d
antonioducs <antonioduarte.cs@gmail.com>
2019-06-16 23:37:12 -0300

    clear

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css"
-
-const Button = props =>(
-    <button className="btn" onClick={props.onClick}>{props.children}</button>
-);
-
-export default Button;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./pages/Main"; // nova importação da página Main
-
-
-ReactDOM.render(<Main />, document.getElementById("root"));
