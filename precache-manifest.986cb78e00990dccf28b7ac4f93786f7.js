83b64fd392087e5c19b9a4d7b1c2b3d60c6de114
antonioducs <antonioduarte.cs@gmail.com>
2019-06-16 23:41:48 -0300

    Updates

---
+++ precache-manifest.986cb78e00990dccf28b7ac4f93786f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4827e8fc37a7cee6e6599ef91b1dde",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "c30f3fc0d480e483f38f",
+    "url": "/portifolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portifolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c30f3fc0d480e483f38f",
+    "url": "/portifolio/static/js/main.f9001fdb.chunk.js"
+  },
+  {
+    "revision": "91a74ff0904045e04265",
+    "url": "/portifolio/static/js/runtime~main.e9cc7322.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
