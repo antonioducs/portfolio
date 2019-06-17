741129368c3703a216ab40dc9ff881d630fd6d95
antonioducs <antonioduarte.cs@gmail.com>
2019-06-16 23:44:03 -0300

    updates

---
+++ src/components/Section/SectionContato/index.js
@@ -0,0 +1,53 @@
+import React, { Component, Fragment } from "react";
+import Button from "../../Button"
+import "./styles.css";
+
+class SectionContato extends Component {
+
+    state = {
+        contato: {
+            email: "antonioduarte.cs@gmail.com",
+            telefone: "+55 (067) 998 493 707",
+            face: "https://www.facebook.com/antonio.cesarduarte.3",
+            insta: "https://www.instagram.com/antonioduarte.cs/",
+            imgFace: "https://github.com/antonioducs/proweb/blob/master/img/portifolio/facebook.png?raw=true",
+            imgInsta: "https://github.com/antonioducs/proweb/blob/master/img/portifolio/instagram.png?raw=true"
+        }
+    };
+
+    onClick = () => {
+        console.log("Alguém clicou!");
+    };
+    render() {
+        return (
+            <Fragment>
+                <div id="Contato">
+                    <h1>Contato</h1>
+                    <div className="item">
+                        <h2>Entre em contato</h2>
+                        <p>Entre em contato pelo formulário, pelo e-mail ou pelo telefone.</p>
+                        <p>{this.state.contato.email}</p>
+                        <p>{this.state.contato.telefone}</p>
+                    </div>
+
+                    <input type="text" id="nome" placeholder="Nome" />
+                    <input type="text" id="email" placeholder="E-mail" />
+                    <input type="text" id="assunto" placeholder="Assunto" />
+                    <textarea placeholder="Mensagem"></textarea>
+
+                    <Button onClick={this.onClick}>Enviar</Button>
+
+                    <div className="redes_sociais">
+                        <h3>Redes sociais</h3>
+                        <p>Entre em contato comigo por minhas redes sociais.</p>
+
+                        <a href={this.state.contato.insta} target="_blank"><img src={this.state.contato.imgInsta} alt="instagram"></img></a>
+                        <a href={this.state.contato.face} target="_blank"><img src={this.state.contato.imgFace} alt="facebook"></img></a>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default SectionContato;
---
+++ src/components/Section/SectionHome/index.js
@@ -0,0 +1,74 @@
+import React, { Component, Fragment } from "react";
+import Button from "../../Button";
+import List from "../../List";
+import "./styles.css";
+
+
+class SectionHome extends Component {
+
+    state = {
+        imgPerfil: "https://github.com/antonioducs/proweb/blob/master/img/portifolio/perfil.jpg?raw=true",
+        lists: [
+            {
+                "title": "Interesses",
+                "img": "https://github.com/antonioducs/proweb/blob/master/img/portifolio/interesses.png?raw=true",
+                "description": ["Desenvolvimento Web",
+                    "Desenvolvimento Mobile",
+                    "Inteligência Artificial",
+                    "Microcontroladores"]
+            },
+            {
+                "title": "Idiomas",
+                "img": "https://github.com/antonioducs/proweb/blob/master/img/portifolio/idiomas.png?raw=true",
+                "description": ["Português - Nativo",
+                    "Inglês - Básico"]
+            },
+            {
+                "title": "Formação",
+                "img": "https://github.com/antonioducs/proweb/blob/master/img/portifolio/formacao.png?raw=true",
+                "description": ["UTFPR - Engenharia de Computação - Cursando",
+                    "2013 - Ensino Médio"]
+            },
+            {
+                "title": "Atividades",
+                "img": "https://github.com/antonioducs/proweb/blob/master/img/portifolio/atividades.png?raw=true",
+                "description": ["Desenvolvimento Web",
+                    "Desenvolvimento Mobile",
+                    "Desenvolvimento de aplicações em Hardware"]
+            }
+        ]
+    };
+    onClick = () => {
+        console.log("Alguém clicou!");
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div id="List">
+                    <div id="ListDir">
+                        <div className="perfilImg">
+                            <img src={this.state.imgPerfil} alt="perfil"></img>
+                        </div>
+                        <h2> Olá, sou
+                        <span className="Nome"> Antonio César Duarte</span>
+                            . Estudante de Engenharia de Computação pela UTFPR-CP,
+                        nasci em Paranaíba - MS, atualmente moro em Cornélio Procópio - PR.</h2>
+
+                        <Button onClick={this.onClick}>CURRÍCULO COMPLETO</Button>
+                    </div>
+                    <div id="ListEsq">
+                        <h1 className="item-1">Antonio Cesar Duarte</h1>
+                        {this.state.lists.map(item => {
+                            return (
+                                <List description={item.description} Imagem={item.img}>{item.title}</List>
+                            );
+                        })}
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default SectionHome;
